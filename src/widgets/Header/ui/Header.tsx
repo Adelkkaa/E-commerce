0b847d0293a73005f6fa3c08afd4577c697d6fc6
Adel <snakeadel00@gmail.com>
2024-05-22 10:51:12 +0300

    add animation for header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -4,6 +4,7 @@ import { HeaderInput } from "@/features/HeaderInput";
 import mainLogo from "@/shared/assets/images/MainLogo.jpg";
 import ProfileIcon from "@/shared/assets/images/Profile.svg";
 import CartIcon from "@/shared/assets/images/ShoppingCart.svg";
+import { Button } from "@/shared/ui";
 import { headerLinks } from "../../../shared/constants/navigationLinks";
 
 export const Header = () => {
@@ -30,8 +31,19 @@ export const Header = () => {
         </nav>
         <HeaderInput />
         <div className="flex gap-[20px] items-center">
-          <CartIcon className="cursor-pointer " />
-          <ProfileIcon className="cursor-pointer" />
+          <Link
+            to="/cart"
+            className="hover:strokeBlue w-[36px] h-[36px] flex justify-center items-center"
+          >
+            <CartIcon className="cursor-pointer w-[20px] h-[20px]" />
+          </Link>
+          <Button
+            variant="icon"
+            size="icon"
+            className="hover:strokeBlue w-[36px] h-[36px]"
+          >
+            <ProfileIcon className="cursor-pointer" />
+          </Button>
         </div>
       </div>
     </header>
