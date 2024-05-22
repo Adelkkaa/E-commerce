fc8adfba649b97c917e3851bd630fdcdfe8de0c2
Adel <snakeadel00@gmail.com>
2024-05-22 08:31:43 +0300

    fix styles for skeleton

--- src/entities/ProdductCardSkeleton/ui/ProductCardSkeleton.tsx
+++ src/entities/ProdductCardSkeleton/ui/ProductCardSkeleton.tsx
@@ -2,7 +2,7 @@ import Skeleton from "react-loading-skeleton";
 
 export const ProductCardSkeleton = () => {
   return (
-    <div className="w-[230px] min-h-[288px] bg-whiteCustom p-3 pb-2">
+    <div className="w-[230px] min-h-[288px] bg-whiteCustom p-3 pb-2 rounded-lg">
       <Skeleton className="h-[212px]" />
       <Skeleton className="h-[76px]" />
     </div>
