de6d9052df6c7519b7fdbd7b565006553e514e35
Adel <snakeadel00@gmail.com>
2024-05-22 15:32:23 +0300

    fix styles for policy page

--- src/pages/Policy/ui/Policy.tsx
+++ src/pages/Policy/ui/Policy.tsx
@@ -4,7 +4,19 @@ import { fillingPolicy } from "../constants/fillingPolicy";
 export const Policy = () => {
   return (
     <section className="mt-[40px] flex flex-col gap-10">
-      <Typography variant="textM">{fillingPolicy.title}</Typography>
+      <Typography variant="textM">
+        Настоящая Политика конфиденциальности персональной информации (далее —
+        Политика конфиденциальности) действует в отношении всей информации,
+        которую CASE (далее — Оператор) может получить от Пользователя сети
+        Интернет (далее — Пользователь) во время использования им Сайта,
+        размещённого в сети Интернет по адресу{" "}
+        <span className="text-red-600">'https://'</span> (далее — Сайт).
+        Использование Сайта означает безоговорочное согласие с настоящей
+        Политикой конфиденциальности и указанными в ней условиями обработки его
+        персональной информации. В случае несогласия с этими условиями
+        Пользователь должен воздержаться от предоставления своих персональных
+        данных Оператору.,
+      </Typography>
       <ol className="flex flex-col gap-10">
         {fillingPolicy.subtitles.map((title, index) => (
           <li key={index}>
